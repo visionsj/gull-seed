4a8029c9cf452cbfccac87e0c7ea14e2b70f15bb
[54545787@qq.com] <[emafsfsf@qq.com]>
2017-10-25 10:32:42 +0800

    修改打包路径

--- dist/nuxt.js
+++ dist/nuxt.js
@@ -1591,7 +1591,7 @@ Options.defaults = {
   mode: 'universal',
   dev: process.env.NODE_ENV !== 'production',
   debug: undefined, // Will be equal to dev if not provided
-  buildDir: '.nuxt',
+  buildDir: '.gull',
   cacheDir: '.cache',
   nuxtAppDir: path.resolve(__dirname, '../lib/app/'), // Relative to dist
   build: {
@@ -1600,7 +1600,7 @@ Options.defaults = {
     extractCSS: false,
     cssSourceMap: undefined,
     ssr: undefined,
-    publicPath: '/_nuxt/',
+    publicPath: '/_gull/',
     filenames: {
       css: 'common.[contenthash].css',
       manifest: 'manifest.[hash].js',
